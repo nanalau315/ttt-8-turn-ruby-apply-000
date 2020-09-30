900a80f86b657fbfa578f8112161e3cd21947d06
Chamaeleonidae Na <nanalau315@hotmail.com>
2020-09-30 01:23:15 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -52,7 +52,7 @@ def turn(board)
   
   index = input_to_index(user_input)
   
-  if valid_move? (board, index)
+  if valid_move? (board, index) == true
     move(board, index, character = "X")
     display_board(board)
   
